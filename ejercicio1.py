910ebfc71b5c3f2b4b017daa9b9ea7b3e1d77443
Jorge Mauricio <jorge.mauricio@me.com>
2017-11-16 10:54:56 -0600

    agregar nueva función

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,24 @@
 def multiplicarVariables(x, y):
 	return x * y
 
+def variableMayor(x,y):
+	'''
+	Regresa el comparativo de las variables
+	mayor, menor o igual
+	param: x : valor de x
+	param: y : valor de y
+	'''
+	if x == y:
+		print("x y y son iguales")
+	elif x > y:
+		print("x es mayor a y")
+	else:
+		print("y es mayor a x")
+
 x = int(input("Dame el valor de x: "))
 y = int(input("Dame el valor de y: "))
 
 print("La multiplicación de x = {} y y = {} es: {}".format(x,y, multiplicarVariables(x,y))) 
+
+# imprimir la variable mayor
+variableMayor(x,y)
