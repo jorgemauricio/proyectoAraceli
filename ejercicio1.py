782c4c381cb1f14167b8d6da0f5bd1b90bc3de9a
Jorge Mauricio <jorge.mauricio@me.com>
2017-11-16 10:47:33 -0600

    agregar ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+def multiplicarVariables(x, y):
+	return x * y
+
+x = int(input("Dame el valor de x: "))
+y = int(input("Dame el valor de y: "))
+
+print("La multiplicación de x = {} y y = {} es: {}".format(x,y, multiplicarVariables(x,y))) 
